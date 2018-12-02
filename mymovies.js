ec6d9dc67895363e17e03806c82cab6ec49f5a3a
Can <mca199667@gmail.com>
2018-12-02 22:24:40 +0300

    Choose different genres and filter movies by name, year, director etc. & Random movie button

--- mymovies.js
+++ mymovies.js
@@ -2,7 +2,7 @@ var actionMoviesArray = [
   ["Action Name1", "1991", "Director1", "Actors1", "Summary1", "5.1", "Good1", "trailer1"],
   ["Action Name2", "1992", "Director2", "Actors2", "Summary2", "5.2", "Good2", "trailer2"],
   ["Action Name3", "1993", "Director3", "Actors3", "Summary3", "5.3", "Good3", "trailer3"],
-  ["Action Name4", "1994", "Director4", "Actors4", "Summary4", "5.4", "Good4", "trailer4"],
+  ["actlala", "1994", "Director4", "Actors4", "Summary4", "5.4", "Good4", "trailer4"],
   ["Action Name5", "1995", "Director5", "Actors5", "Summary5", "5.5", "Good5", "trailer5"]
 ];
 
@@ -46,6 +46,12 @@ var adventureMoviesArray = [
   ["Adventure Name5", "1995", "Director5", "Actors5", "Summary5", "5.5", "Good5", "trailer5"]
 ];
 
+// Define filter inputs
+var movieFilter = document.getElementById("movie-filter");
+
+var chosenGenreItemsArray;
+
+
 // Specify which array to be displayed with the clicked anchor id
 function chooseGenre(clickedGenre) {
   if(clickedGenre == "action") {
@@ -71,12 +77,41 @@ function displayMovies(arrayName) {
     for(var i=0; i<arrayName.length; i++) {
       var li = document.createElement("li");
       li.classList.add("chosen-genre-item");
-      li.innerHTML = "<strong>Movie name: </strong>" + arrayName[i][0] + " <strong>Year: </strong>" + arrayName[i][1] + " <strong>Director: </strong> " + arrayName[i][2] + " <strong>Actors/Actresses: </strong> " + arrayName[i][3] + " <strong>Summary: </strong> " + arrayName[i][4] + " <strong>IMDB Rate: </strong> " + arrayName[i][5] + " <strong>My Comment: </strong> " + arrayName[i][6] + " <strong>Trailer: </strong> <a href='" + arrayName[i][7] + "'>Trailer</a>";
+      li.innerHTML = "<strong>Movie name: </strong>" + arrayName[i][0] + " <strong>Year: </strong>" + arrayName[i][1] + " <strong>Director: </strong> " + arrayName[i][2] + " <strong>Actors/Actresses: </strong> " + arrayName[i][3] + " <strong>Summary: </strong> " + arrayName[i][4] + " <strong>IMDB Rate: </strong> " + arrayName[i][5] + " <strong>My Comment: </strong> " + arrayName[i][6] + " <strong>Trailer: </strong> <a href='" + arrayName[i][7] + "' class='trailer-link'>Trailer</a>";
       ul.appendChild(li);
     }
     movieListDiv.appendChild(ul);
   } else {
     movieListDiv.innerHTML = "";
     displayMovies(arrayName);
+  }
+  chosenGenreItemsArray = document.getElementsByClassName("chosen-genre-item");
+  var itemContent = "";
+}
+
+// Get inputs from filter tools
+movieFilter.onkeyup = function() {
+  // Filter movie names
+  var filterInput = movieFilter.value;
+  for(var i = 0; i < chosenGenreItemsArray.length; i++) {
+    itemContent = chosenGenreItemsArray[i].innerText;
+    if((itemContent.toLowerCase()).includes(filterInput.toLowerCase())) {
+      chosenGenreItemsArray[i].hidden = false;
+    } else {
+      chosenGenreItemsArray[i].hidden = true;
+    }
+  }
+}
+
+document.getElementById("random-movie-button").onclick = function() {
+  var movieList = document.getElementById("movie-list-div");
+  if(movieList.innerHTML == "") {
+    alert("Choose a category first.");
+  } else {
+    var rand = Math.floor(Math.random() * chosenGenreItemsArray.length);
+    for(var i=0; i<chosenGenreItemsArray.length; i++) {
+      chosenGenreItemsArray[i].hidden = true;
+    }
+    chosenGenreItemsArray[rand].hidden = false;
   }
 }
